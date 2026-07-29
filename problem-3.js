078969c23d795efaa241ae21a1cbbce316e8e326
890asif <192508892+890asif@users.noreply.github.com>
2026-07-30 00:13:44 +0600

    calculate cost

--- problem-3.js
+++ problem-3.js
@@ -10,7 +10,10 @@ function calculateAiCost(tokensUsed) {
     let rate = total * 5;
     return rate;
 }
+console.log(calculateAiCost(300))
 console.log(calculateAiCost(500))
 console.log(calculateAiCost(650))
-console.log(calculateAiCost(-650))
-console.log(calculateAiCost("650"))
+console.log(calculateAiCost(1000))
+console.log(calculateAiCost(-10))
+console.log(calculateAiCost("500"))
+
