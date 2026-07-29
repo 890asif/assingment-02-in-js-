0c69cc26450aba1e6cbcbd4fc329f05ef7a36767
890asif <192508892+890asif@users.noreply.github.com>
2026-07-30 00:06:27 +0600

    calculate cost

--- problem-3.js
+++ problem-3.js
@@ -1,16 +1,16 @@
-// function calculateAiCost(tokensUsed) {
-//     if(typeof tokensUsed !== "number" || tokensUsed < 0){
-//         return  "Invalid";l
-//     }
-//     if(tokensUsed <= 500){
-//         return 0;
-//     }
-//     let totalTokenUsed = tokensUsed - 500;
-//     let total = Math.floor(totalTokenUsed / 100);
-//     let rate = total * 5;
-//     return rate;
-// }
-// console.log(calculateAiCost(500))
-// console.log(calculateAiCost(650))
-// console.log(calculateAiCost(-650))
-// console.log(calculateAiCost("650"))
+function calculateAiCost(tokensUsed) {
+    if(typeof tokensUsed !== "number" || tokensUsed < 0){
+        return  "Invalid";l
+    }
+    if(tokensUsed <= 500){
+        return 0;
+    }
+    let totalTokenUsed = tokensUsed - 500;
+    let total = Math.floor(totalTokenUsed / 100);
+    let rate = total * 5;
+    return rate;
+}
+console.log(calculateAiCost(500))
+console.log(calculateAiCost(650))
+console.log(calculateAiCost(-650))
+console.log(calculateAiCost("650"))
